c32acc86baa4dfbd1005ea330abe7b677df115eb
Hendrik Lambert <hendrik@lambert.biz>
2019-09-22 15:24:10 +0200

    test

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print("This is assignment 1")
-print("change 1")
-print("change 2")
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(-10, 10, .1)
+
+
+def y(val):
+    return val**5
+
+
+plt.plot(x, y(x))
+plt.grid()
+plt.xlabel("x (unit of x)")
+plt.ylabel("y (unit of y)")
+plt.title('y vs x')
+plt.show()
